1536cbec3eb37677ecd516f6c8e4e9701e480a28
hanksnowdon <hanksnowdon>
2024-12-05 22:09:14 +0000

    Started Data Prep

--- notebooks/EDA.py
+++ notebooks/EDA.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 df = spark.read.table("hive_metastore.default.final_depression_dataset")
-df = df.toPandas()  # If small enough, else continue with Spark DataFrame methods
+df = df.toPandas()  
 
 df.head()
 
